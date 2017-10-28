7bd5716c056cad04eb7fe25a41447e1e868f6c17
Christopher Gu <clg2n4@mst.edu>
2017-10-28 01:08:29 -0500

    fixed java.lang.ClassCastException error

--- app/src/main/java/com/example/cleeg/lighthouse/MyApplication.java
+++ app/src/main/java/com/example/cleeg/lighthouse/MyApplication.java
@@ -6,14 +6,13 @@ import com.estimote.coresdk.common.config.EstimoteSDK;
 import com.example.cleeg.lighthouse.estimote.BeaconNotificationsManager;
 
 public class MyApplication extends Application {
+
     private boolean beaconNotificationsEnabled = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // TODO: put your App ID and App Token here
-        // You can get them by adding your app on https://cloud.estimote.com/#/apps
         EstimoteSDK.initialize(getApplicationContext(), "lighthouse-8u5", "8af68c0e454258ec2261aea53c15a950");
         //EstimoteSDK.enableDebugLogging(true);
     }
