344cbb52da5bc8a559da75345006412fd8c2bc89
Christopher Gu <clg2n4@mst.edu>
2017-10-29 05:02:27 -0500

    attempting to start parsing google calendar event information...

---
+++ app/src/main/java/com/example/cleeg/lighthouse/calendar/QueryUtilsDos.java
@@ -0,0 +1,14 @@
+package com.example.cleeg.lighthouse.calendar;
+
+public class QueryUtilsDos {
+
+    // Tag for the log messages
+    private static final String TAG = "QueryUtilsDos";
+
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtilsDos} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtilsDos() {}
+}
